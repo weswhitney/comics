6b97f96f0eabf3736c479d93a88de35f0500ea93
weswhitney <wesleywhitney@gmail.com>
2016-12-08 22:11:49 -0700

    changed home route to characters

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', characters);
 app.use('/characters', characters);
 app.use('/comics', comics);
 
